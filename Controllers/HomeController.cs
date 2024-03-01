9ba9eeb96d7fa761f70dde7c74feae538144a1d0
Gurlinkaur23 <gurlinkaur338@gmail.com>
2024-02-29 20:50:44 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using EMBLL;
+using EMENTITIES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Event_Management.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// It displays the list of events
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            EventsService es = new EventsService();
+            var events = es.GetEventsService();
+
+            return View(events);
+        }
+
+        /// <summary>
+        /// It creates a form to add a new event. If successful, it displays a confirmation message for the user.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult CreateEventsView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult CreateEventsView(Events newEvent)
+        {
+            EventsService es = new EventsService();
+            if (es.AddEventService(newEvent))
+            {
+                ViewBag.Message = "The Event is added successfully.";
+            }
+            return View();
+        }
+
+        /// <summary>
+        /// It returns a view with a form to update the event information with the record based on EventId.
+        /// </summary>
+        /// <param name="eventId"></param>
+        /// <returns></returns>
+        public ActionResult EditEventsView(int eventId)
+        {
+            EventsService es = new EventsService();
+
+            return View(es.GetEventsService().Find(x => x.EventId == eventId));
+        }
+
+        /// <summary>
+        /// This method updates the event. If successful, it displays a confirmation message for the user.
+        /// </summary>
+        /// <param name="updatedEvent"></param>
+        /// <returns></returns>
+
+        [HttpPost]
+
+        public ActionResult EditEventsView(Events updatedEvent)
+        {
+            EventsService es = new EventsService();
+
+            if (es.UpdateEventService(updatedEvent))
+            {
+                ViewBag.Message = "The event has been updated successfully.";
+            }
+            return View();
+        }
+
+        /// <summary>
+        /// This method deletes an event from the DB based on eventId.
+        /// </summary>
+        /// <param name="eventId"></param>
+        /// <returns></returns>
+        public ActionResult DeleteEvent(int eventId)
+        {
+            EventsService es = new EventsService();
+
+            if (es.DeleteEventService(eventId))
+            {
+                ViewBag.Message = "Event is deleted successfully.";
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,133 @@
+﻿using EMBLL;
+using EMENTITIES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Event_Management.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        /// <summary>
+        /// It displays the list of registrations for the particualar event.
+        /// </summary>
+        /// <param name="eventId"></param>
+        /// <returns></returns>
+        public ActionResult Index(int eventId)
+        {
+            RegistrationsService rs = new RegistrationsService();
+            var registrations = rs.GetRegistrationsService(eventId);
+
+            TempData["eventId"] = eventId;
+
+            return View(registrations);
+        }
+
+        /// <summary>
+        /// This method creates a new registration in the DB.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult CreateRegistrationsView()
+        {
+            // Retrieve events from the database
+            EventsService es = new EventsService();
+            var events = es.GetEventsService();
+
+            // Pass the events to the view
+            ViewBag.EventId = new SelectList(events, "EventId", "EventName");
+
+            // Create a new registration object
+            Registrations newRegistration = null;
+
+            return View(newRegistration);
+        }
+
+        /// <summary>
+        /// This method adds a new registration based on the selected event from the dropdown.
+        /// </summary>
+        /// <param name="newRegistration"></param>
+        /// <param name="eventId"></param>
+        /// <returns></returns>
+        [HttpPost]
+
+        public ActionResult CreateRegistrationsView(Registrations newRegistration, int eventId)
+        {
+            // Set the EventId property of the new registration object based on the selected event
+            newRegistration.EventId = eventId;
+
+            // Create an instance of EventsService
+            EventsService es = new EventsService();
+
+            // Call GetEventsService upon es and store it inside events
+            var events = es.GetEventsService();
+
+            var listOfEventsDropdown = events.Select(e => new SelectListItem
+            {
+                Text = e.EventName,
+                Value = e.EventId.ToString()
+            }).ToList();
+
+            // Use TempData to store it in listOfEventsDropdown
+            TempData["listOfEventsDropdown"] = listOfEventsDropdown;
+            TempData.Keep();
+
+            // Save the new registration to the database
+            RegistrationsService rs = new RegistrationsService();
+            if (rs.AddRegistrationService(newRegistration))
+            {
+                ViewBag.Message = "The registration has been added successfully.";
+            }
+
+            return View(newRegistration);
+        }
+
+        /// <summary>
+        /// It returns a view with a form to update the registration information with the record based on registrationId.
+        /// </summary>
+        /// <param name="eventId"></param>
+        /// <param name="registrationId"></param>
+        /// <returns></returns>
+        public ActionResult EditRegistrationsView(int eventId, int registrationId)
+        {
+            RegistrationsService rs = new RegistrationsService();
+
+            return View(rs.GetRegistrationsService(eventId).Find(x => x.RegistrationId == registrationId));
+        }
+
+        /// <summary>
+        /// This method updates the event. If successful, it displays a confirmation message for the user.
+        /// </summary>
+        /// <param name="registration"></param>
+        /// <returns></returns>
+        [HttpPost]
+
+        public ActionResult EditRegistrationsView(Registrations registration)
+        {
+            RegistrationsService rs = new RegistrationsService();
+
+            if (rs.UpdateRegistrationService(registration))
+            {
+                ViewBag.Message = "Registration has been updated successfully.";
+            }
+            return View(registration);
+        }
+
+        /// <summary>
+        /// This method deletes a registration from the DB based on registrationId.
+        /// </summary>
+        /// <param name="registrationId"></param>
+        /// <returns></returns>
+        public ActionResult DeleteRegistration(int registrationId)
+        {
+            RegistrationsService rs = new RegistrationsService();
+
+            if (rs.DeleteRegistrationtService(registrationId))
+            {
+                return RedirectToAction("Index", new { eventId = TempData["eventId"] });
+            }
+            return null;
+        }
+    }
+}
